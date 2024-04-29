3ef06dfcf2284c14283d5df74466c0474bf6225a
shirleyxueli41 <shirleyxueli41@gmail.com>
2024-04-29 09:58:06 -0400

    add fetchngs.sh.

---
+++ nf-core/nfcore-fetchngs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -J fetchngs
+#SBATCH --time=00-12:00:00
+#SBATCH -p batch
+#SBATCH -n 2
+#SBATCH --mem=8g
+#SBATCH --output=MyJob.%j.%N.out
+#SBATCH --error=MyJob.%j.%N.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=xue.li37@tufts.edu
+
+
+module load  nf-core-fetchngs/1.12.0 
+
+fetchngs -profile tufts \
+	    --input ids.csv \
+        --download_method aspera \
+        --nf_core_pipeline rnaseq \
+        --outdir ./fetchngsout
+
